53f5948cff3d16cda91d83a416daf0636e3aae0c
Tanay Sharma <thisistanaysharma@gmail.com>
2025-02-23 16:52:14 +0530

    Added docker files

--- week-27-todo-app/apps/web/app/page.tsx
+++ week-27-todo-app/apps/web/app/page.tsx
@@ -4,3 +4,7 @@ export default async function Home() {
   const users = await prismaClient.user.findMany();
   return <div>{JSON.stringify(users)}</div>;
 }
+
+// export const revalidate = 60 // revalidate every 60 seconds
+// or
+// export const dynamic = "force-dynamic";
